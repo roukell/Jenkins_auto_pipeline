ffd5361cc28172989fae7dfd3365324130dc11c9
Melody Lo <roukell@hotmail.com>
2023-09-13 12:10:55 +1000

    Update create_api_token.sh

--- create_api_token.sh
+++ create_api_token.sh
@@ -12,6 +12,8 @@ curl 'http://localhost:8080/user/admin/descriptorByName/jenkins.security.ApiToke
 // example output
 {"status":"ok","data":{"tokenName":"kb-token","tokenUuid":"f2319160-f159-4397-9aaf-a9ec6ccb5931","tokenValue":"119ba16ffcd0ab29676e99ccffaf5c433a"}}
 
------WORKING SCRIPT-----
+//-----WORKING SCRIPT-----
+sed -i 's/^Environment="JAVA_OPTS=.*/Environment="JAVA_OPTS=-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false"/' /usr/lib/systemd/system/jenkins.service
+ADMIN_PASSWORD=$(cat /var/jenkins_home/secrets/initialAdminPassword)
 CRUMB=$(curl "http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)" --cookie-jar cookies.txt --user admin:$ADMIN_PASSWORD)
 ACCESS_TOKEN=$(curl 'http://localhost:8080/user/admin/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken' --user admin:$ADMIN_PASSWORD --data 'newTokenName=ec2-token' --cookie cookies.txt -H $CRUMB | jq -r '.data.tokenValue')
